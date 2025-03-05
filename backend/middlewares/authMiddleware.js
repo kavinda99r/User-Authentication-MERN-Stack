d1e0cc1d28aa21e905a0391ee6ab1b0a630c0e8d
kavinda99r <kavinda99ac@gmail.com>
2025-03-05 07:56:10 +0530

    Modify code readability

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -9,9 +9,7 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    
     req.userId = verified.userId;
-
     next();
   } catch (error) {
     res.status(400).json({ message: "Invalid Token" });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import ProfilePage from "./pages/ProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
       </div>
     </>
   );
